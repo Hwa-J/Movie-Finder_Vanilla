a3c92b48885b684e7de2cfedbc7f0821fa59f5c7
Hwa-J <hj1103.code@gmail.com>
2022-05-13 16:05:15 +0900

    refactor: 검색 결과물 출력 방법 수정

--- js/main.js
+++ js/main.js
@@ -1,38 +1,22 @@
 import '../scss/main.scss'
+import modalError from './moda_error'
 
 const searchBar = document.querySelector('.search-bar')
 const searchForm = document.querySelector('.search__form')
 
 
-async function getMovie(title) {
+async function getMovie(title, num=1) {
   const OMDB_API_KEY = '7035c60c'
-
-
-  
-    let res = await fetch(`https://www.omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}`)
+    let res = await fetch(`https://www.omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}&page=${num}`)
     res = await res.json()
     console.log(res)
+    res.Search.forEach((i,index) => console.log(i,index))
     try {
-    // for (let i = 0; i < res.length; += i) += 연산자 에러 남
-    for (let i = 0; i < res.Search.length; ++ i) {
-      let movie = document.querySelector(`.movie_${ i + 1 }`)
-      let movieInfo = movie.querySelector(`.info_${ i + 1 }`)
+    await renderMovie(res)
 
-      // 변수 선언 생략 가능하지만 유지보수를 위해 남겨둬야 할까?
-      let resPoster = res.Search[i].Poster
-      let resTitle = res.Search[i].Title
-      let resYear = res.Search[i].Year
-      
-      movie.children[0].src = resPoster
-      movie.children[0].alt = resTitle
-      movieInfo.children[0].textContent = resTitle
-      movieInfo.children[1].textContent = `(${resYear})`
-    }
-    console.log(res)
     return res
   } catch(error) {
-    console.log(error)
-    alert(`${error}`)
+    modalError()
   }
 }
 
@@ -44,8 +28,37 @@ searchForm.addEventListener("submit", async (e) => {
     await getMovie(`${value}`)
   }
   catch(error) {
-    console.log(error)
-    alert('재검색 해주세요!')
+    modalError()
   }
 })
 
+function renderMovie (res) {
+  res.Search.forEach( (resMovie, index) => {
+    
+    createItem(resMovie, index)
+
+    let movie = document.querySelector(`.movie_${ index + 1 }`)
+    let movieInfo = movie.querySelector(`.info_${ index + 1 }`)
+
+    movie.children[0].src = resMovie.Poster
+    // movie.children[0].alt = resMovie.Title
+    movieInfo.children[0].textContent = resMovie.Title
+    movieInfo.children[1].textContent = `(${resMovie.Year})`
+  })
+}
+
+function createItem (resMovie, index) {
+  const movieList = document.querySelector('.movies')
+  const liEl = document.createElement('li')
+  liEl.classList.add('movie')
+  liEl.innerHTML = `
+  <a href="javascript:void(0)" class="movie_${ index + 1 }">
+    <img src="" alt="${ resMovie.Title }" class="movie__poster">
+    <div class="movie__info info_${ index + 1 }">
+      <div class="movie__info--title"></div>
+      <div class="movie__info--year"></div>
+    </div>
+  </a>
+  `
+  movieList.append(liEl)
+}
