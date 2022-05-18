25487a596ac9825e4c8531e42e0e41e5660c99cd
Hwa-J <hj1103.code@gmail.com>
2022-05-19 02:46:51 +0900

    refactor: 모달창의 닫기 버튼에 대한 iOS 모바일 환경 click event 개선

--- js/modal.js
+++ js/modal.js
@@ -14,9 +14,17 @@ export function modalMovie () {
 export function modalClose () {
   const close = document.querySelectorAll('.modal__close')
 
+  // touch device 지원 확인 함수 (iOS 모바일 기기 click event 오류 개선용)
+  let clickEvent = (() => {
+    if ('ontouchstart' in document.documentElement === true) {
+      return 'touchstart';
+    } else {
+      return 'click';
+    }})();
+
   for (let btn of close) {
-    btn.addEventListener('click', (modalBox) => {
-      // 모달창의 조상요소의 classList에 'hidden'추가
-      modalBox.path[4].classList.add('hidden')})
+    btn.addEventListener( clickEvent , (btnClose) => {
+      // X버튼 클릭시 모달창의 조상요소의 classList에 'hidden'추가
+      btnClose.path[4].classList.add('hidden')})
   }
 }
