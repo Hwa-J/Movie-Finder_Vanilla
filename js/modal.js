86b8ee2b864afcf8c13038ce584cab0396eb5efe
Hwa-J <hj1103.code@gmail.com>
2022-05-19 15:03:06 +0900

    feat: 모달창 닫기 기능 향상

--- js/modal.js
+++ js/modal.js
@@ -11,20 +11,37 @@ export function modalMovie () {
   modalClose()
 }
 
+// 모달창 닫기 기능
 export function modalClose () {
-  const close = document.querySelectorAll('.modal__close')
+  const modal = document.querySelectorAll('.modal')
+  // const close = document.querySelectorAll('.modal__close')
 
-  // touch device 지원 확인 함수 (iOS 모바일 기기 click event 오류 개선용)
-  let clickEvent = (() => {
-    if ('ontouchstart' in document.documentElement === true) {
-      return 'touchstart';
-    } else {
-      return 'click';
-    }})();
+  // // touch device 지원 확인 함수 (iOS 모바일 기기 click event 오류 개선용)
+  // let clickEvent = (() => {
+  //   if ('ontouchstart' in document.documentElement === true) {
+  //     return 'touchstart';
+  //   } else {
+  //     return 'click';
+  //   }})();
 
-  for (let btn of close) {
-    btn.addEventListener( clickEvent , (btnClose) => {
-      // X버튼 클릭시 모달창의 조상요소의 classList에 'hidden'추가
-      btnClose.path[4].classList.add('hidden')})
+  // for (let btn of close) {
+  //   btn.addEventListener( 'click' , () => {
+  //     console.log('oh')
+  //     modal.classList.add('hidden')
+  //   })
+  // }
+
+  for (let on of modal) {
+    on.addEventListener('click', (e) => {
+      // 모달창 바깥 영역
+      if (e.target.classList.contains('modal') 
+      // 모달찰 닫기 버튼
+      || e.target.classList.contains('modal__close')) {
+        on.classList.add('hidden')
+      }
+      // else if (e.target.classList.contains('modal__close')) {
+      //   on.classList.add('hidden')
+      // }
+    })
   }
 }
