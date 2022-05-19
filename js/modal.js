2019445ee2ef2f1c5ee982a78554ee270a7cda14
Hwa-J <hj1103.code@gmail.com>
2022-05-19 15:06:42 +0900

    refactor: 주석 정리

--- js/modal.js
+++ js/modal.js
@@ -14,34 +14,16 @@ export function modalMovie () {
 // 모달창 닫기 기능
 export function modalClose () {
   const modal = document.querySelectorAll('.modal')
-  // const close = document.querySelectorAll('.modal__close')
 
-  // // touch device 지원 확인 함수 (iOS 모바일 기기 click event 오류 개선용)
-  // let clickEvent = (() => {
-  //   if ('ontouchstart' in document.documentElement === true) {
-  //     return 'touchstart';
-  //   } else {
-  //     return 'click';
-  //   }})();
-
-  // for (let btn of close) {
-  //   btn.addEventListener( 'click' , () => {
-  //     console.log('oh')
-  //     modal.classList.add('hidden')
-  //   })
-  // }
-
-  for (let on of modal) {
-    on.addEventListener('click', (e) => {
+  for (let open of modal) {
+    open.addEventListener('click', (e) => {
       // 모달창 바깥 영역
       if (e.target.classList.contains('modal') 
       // 모달찰 닫기 버튼
       || e.target.classList.contains('modal__close')) {
-        on.classList.add('hidden')
+
+        open.classList.add('hidden')
       }
-      // else if (e.target.classList.contains('modal__close')) {
-      //   on.classList.add('hidden')
-      // }
     })
   }
 }
